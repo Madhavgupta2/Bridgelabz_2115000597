eef462fd80b8a3adf02686f601937d910ac9f7a3
VaibhavSharmaa633 <your-email@example.com>
2025-01-30 14:08:40 +0530

    update commit

--- 28-01-25/string/RemoveDuplicates.java
+++ 28-01-25/string/RemoveDuplicates.java
@@ -1,17 +1,42 @@
+// import java.util.Scanner;
+// public class RemoveDuplicates{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         String input=sc.nextLine();
+//         System.out.println(removeDuplicates(input));
+//     }
+//     public static String removeDuplicates(String str){
+//         StringBuilder result=new StringBuilder();
+//         for(int i=0;i<str.length();i++){
+//             if(result.indexOf(String.valueOf(str.charAt(i)))==-1){
+//                 result.append(str.charAt(i));
+//             }
+//         }
+//         return result.toString();
+//     }}
+
 import java.util.Scanner;
-public class RemoveDuplicates{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
-        System.out.println(removeDuplicates(input));
-    }
-    public static String removeDuplicates(String str){
-        StringBuilder result=new StringBuilder();
-        for(int i=0;i<str.length();i++){
-            if(result.indexOf(String.valueOf(str.charAt(i)))==-1){
-                result.append(str.charAt(i));
+
+public class removeduplicates {
+    public static String removeDuplicates(String str) {
+        boolean[] seen = new boolean[256];
+        char[] chars = str.toCharArray();
+        int index = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (!seen[chars[i]]) {
+                seen[chars[i]] = true;
+                chars[index++] = chars[i];
             }
         }
-        return result.toString();
+        return new String(chars, 0, index);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        System.out.println("String after removing duplicates: " + removeDuplicates(input));
     }
 }
